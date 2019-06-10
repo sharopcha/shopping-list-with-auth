357cf837e21e4826e8b2c7fb0deaafff10206a58
sharopcha <36384699+sharopcha@users.noreply.github.com>
2019-06-10 17:19:07 +0300

    Craeted shopping list component and added transitio for creating and deleting item actions

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 import AppNavbar from './components/AppNavbar'
+import ShoppingList from './components/ShoppingList'
 
 function App() {
   return (
     <div className="App">
       <AppNavbar />
+      <ShoppingList />
     </div>
   );
 }
